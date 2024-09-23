9db4fdf16ad3c34297fc24322e37186bd0f0c314
Irene Amadi <ireneamadi@ymail.com>
2024-09-23 17:29:56 +0100

    'imported some functions and added the beginnigs of 2 loops'

--- data_create.py
+++ data_create.py
@@ -10,8 +10,40 @@ columns_headers = [
 # dara = 'cholesterol_level, family_diabetes_history, social_media_usage, sleep_duration
 # irene = diabetes_pedigree_function, diet_type, physical_activity_level, 
 
-## Create loops for each feature, to generate 100k rows of randomized data relevant to that feature.
+## Create loops for each feature, to: generate 100k rows of randomized data relevant to that feature, with null values randomly distibuted within each feature.
 
-diet_type_dict = {'vegan', 'paleo', 'carnivore', 'atkins', 'raw food', 'pescatarian', 'gluten free', 'ketogenic', 'mediterranean', 'vegetarian', 'weight watchers', 'low carb'}
+#Diet Type Loop
 
-#Export to Excel
+#import libraries
+#import numpy as np
+#import pandas as pd
+import random as rand
+import string 
+
+#create a loop function 
+def diet_loop(diet_dict):
+    for x in diet_dict:
+        print (x)
+        break 
+#find function to add randomness
+
+#write a dictionary for the     
+diet_dict = {'vegan', 'paleo', 'carnivore', 'atkins', 'raw food', 'pescatarian', 'gluten free', 'ketogenic', 'mediterranean', 'vegetarian', 'weight watchers', 'low carb'}
+
+# diet_df =
+
+#Diabetes Pedigree Fucntion loop
+# generate 100k randomized dataframe, each value ranging between 
+def pedigree_loop(dpf_dict):
+    for x in diet_dict:
+        print (x)
+        break 
+pedigree_df = pd.DataFrame(np.random.randn(100, 4), columns=list('ABCD'))
+
+#Starsign loop
+
+#Physical activity level loop
+
+#Join dataframes
+
+#Export to Excel - possibly import thhe df, and append in python - or could just export, cut and paste.
